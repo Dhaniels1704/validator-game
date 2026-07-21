bfcc954f3c277f1807d07e39a0fd0a2e7e45cd23
Dhaniels1704 <138221538+Dhaniels1704@users.noreply.github.com>
2026-07-21 17:28:37 +0700

    Update routing.ts

--- src/routing.ts
+++ src/routing.ts
@@ -15,7 +15,7 @@ export default async function callAPI(url: string): Promise<Result> {
       case path.includes('/aov'):
         return await router.aov(Number(id))
       case path.includes('/codm'):
-        return await router.codm(Number(id))
+        return await router.codm(id)
       case path.includes('/ff'):
         return await router.ff(Number(id))
       case path.includes('/gi'):
