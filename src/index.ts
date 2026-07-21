87c17045d039f5e79c6fdbe2f4f911f04a14feb9
Dhaniels1704 <138221538+Dhaniels1704@users.noreply.github.com>
2026-07-21 09:35:01 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import checkCache from './handler'
+import { LANDING_HTML, DOCS_HTML } from './pages'
 
 export default {
-  fetch: async (request: Request): Promise<Response> => await checkCache(request)
+  fetch: async (request: Request): Promise<Response> => {
+    const url = new URL(request.url)
+
+    if (url.pathname === '/' || url.pathname === '') {
+      return new Response(LANDING_HTML, {
+        headers: { 'content-type': 'text/html;charset=UTF-8' }
+      })
+    }
+
+    if (url.pathname === '/docs' || url.pathname === '/docs/') {
+      return new Response(DOCS_HTML, {
+        headers: { 'content-type': 'text/html;charset=UTF-8' }
+      })
+    }
+
+    return await checkCache(request)
+  }
 }
